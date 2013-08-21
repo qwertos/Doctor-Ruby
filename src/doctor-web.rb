0c15864cc031896e72a90b2deafb12bc3bf39876
Aaron Herting <qwertos@river.rit.edu>
2013-08-21 19:07:50 -0400

    adding more xmpp code

--- src/doctor-web.rb
+++ src/doctor-web.rb
@@ -5,6 +5,17 @@ require 'xmpp4r/client'
 require 'sinatra'
 require 'erb'
 require '../config/private/web-config.rb'
+include Jabber
+
+
+local_jid = JID.new $SETTINGS[:jid]
+@xmpp_connection = Client.new local_jid
+@xmpp_connection.connect
+@xmpp_connection.auth $SETTINGS[:password]
+@xmpp_connection.send Presence.new
+
+$USER_DB = []
+
 
 get '/' do
 	refresh_local_db
@@ -13,11 +24,9 @@ end
 
 
 def refresh_local_db
-	
+	message = Message.new $SETTINGS[:master_jid], "request.user_db"
 end
 
-def connect_to_xmpp
-	
-end
+
 
 
