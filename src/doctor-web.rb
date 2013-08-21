d2804c3bc78f168467c8fee2245c644386356eaa
Aaron Herting <qwertos@river.rit.edu>
2013-08-21 18:38:23 -0400

    adding xmpp stuffs and ignoring private files

--- src/doctor-web.rb
+++ src/doctor-web.rb
@@ -1,11 +1,23 @@
 #!/usr/bin/ruby
 
 require 'rubygems'
+require 'xmpp4r/client'
 require 'sinatra'
 require 'erb'
+require '../config/private/web-config.rb'
 
 get '/' do
+	refresh_local_db
 	erb :index
+end
+
+
+def refresh_local_db
+	
+end
+
+def connect_to_xmpp
+	
 end
 
 
