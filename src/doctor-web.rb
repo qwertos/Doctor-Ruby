dc09bb601547d3dc86968c0a9a4fa9f9c05fcf67
Aaron Herting <qwertos@river.rit.edu>
2013-08-21 20:13:58 -0400

    more xmpp stuffs

--- src/doctor-web.rb
+++ src/doctor-web.rb
@@ -11,13 +11,31 @@ require '../config/private/web-config.rb'
 include Jabber
 
 
+$user_db = []
+
 local_jid = JID.new $SETTINGS[:jid]
 @xmpp_connection = Client.new local_jid
 @xmpp_connection.connect
 @xmpp_connection.auth $SETTINGS[:password]
 @xmpp_connection.send Presence.new
 
-$USER_DB = []
+@xmpp_connection.add_message_callback do |message|
+	xml = message.body
+	hash = CobraVsMongoose.xml_to_hash xml
+
+	case hash['method']
+		when 'post'
+			handle_xmpp_post hash
+
+		when 'get'
+			handle_xmpp_get hash
+		
+		else
+			puts "Is there another xmpp method"
+	end
+			
+end
+
 
 
 get '/' do
@@ -27,8 +45,32 @@ end
 
 
 def refresh_local_db
-	message = Message.new $SETTINGS[:master_jid], "request.user_db"
+	hash = {
+		"method" => "get",
+		"key" => "user_db"
+	}
+
+	message = Message.new( $SETTINGS[:master_jid], CobraVsMongoose.hash_to_xml( hash ))
+	message.set_type :normal
+	message.set_id '1'
+
+	@xmpp_connection.send message
 end
+
+
+def handle_xmpp_post hash
+	case hash['key']
+		when 'user_db'
+			$user_db = hash['value']
+
+		when 'update_exists'
+			refresh_local_db
+
+		else
+			puts 'Is there another key?'
+	end
+end
+
 
 
 
